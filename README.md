0d71ad3826920e9a919c6ebdcaa5b30b2a71ba18
unknown <annagubar.work@gmail.com>
2024-12-07 21:29:02 +0200

    reviews sglite3

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-`npm init`
-`npm i -save-dev express`
-`npm i --save-dev logger-service`
-`npm update` - обновление пакетов
-`npm uninstall -пакет-`
+## npm init
+
+## npm i -save-dev express
+
+## npm i --save-dev logger-service
+
+## npm update / npm update -пакет-
+
+## npm uninstall -пакет-
+
+## npm i body-parser
+
+## npm i sqlite3
--- server.js
+++ server.js
@@ -1,62 +1,125 @@
-const http = require("http");
+const express = require("express");
+const bodyParse = require("body-parser");
+const app = express();
+const port = 3000;
+const sqlite3 = require("sqlite3").verbose();
+const dbName = "tasks.db";
+const db = new sqlite3.Database(dbName);
 
-const host = "127.0.0.1";
-const port = 4444;
+let tasks = [
+  {
+    id: 1,
+    text: "bkq tw",
+  },
+  {
+    id: 2,
+    text: " tsrhe h ewh e",
+  },
+  {
+    id: 3,
+    text: "fffffbkq rthw",
+  },
+  {
+    id: 4,
+    text: "wc4jweeeeeeeeeeeeeeeh",
+  },
+  {
+    id: 5,
+    text: "hwwwww qhq",
+  },
+];
 
-function notFound(res) {
-  res.statusCode = 404;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Not Found\n");
-}
-
-const server = http.createServer((req, res) => {
-  switch (req.method) {
-    case "GET":
-      switch (req.url) {
-        case "/home":
-          res.statusCode = 200;
-          res.setHeader("Content-Type", "text/plain");
-          res.end("Home Page\n");
-          break;
-
-        case "/about":
-          res.statusCode = 200;
-          res.setHeader("Content-Type", "text/plain");
-          res.end("About Page\n");
-          break;
-
-        default:
-          notFound(res);
-          break;
-      }
-      break;
-
-    case "POST":
-      switch (req.url) {
-        case "/api/admin":
-          res.statusCode = 200;
-          res.setHeader("Content-Type", "text/plain");
-          res.end("Create admin request\n");
-          break;
-
-        case "/api/user":
-          res.statusCode = 200;
-          res.setHeader("Content-Type", "text/plain");
-          res.end("Create user request\n");
-          break;
-
-        default:
-          notFound(res);
-          break;
-      }
-      break;
-
-    default:
-      notFound(res);
-      break;
+const serverError = (err, rows) => {
+  if (err) {
+    return res.status(500).json({ error: err.message });
   }
+};
+
+const checkExist = (task, res) => {
+  if (!task) {
+    return res.status(404).json({ message: "Task is not found" });
+  }
+};
+
+app.use(bodyParse.json());
+
+app.get("/", (req, res) => {
+  return res.send("Hello");
 });
 
-server.listen(port, host, () => {
-  console.log(`Server listening http://${host}:${port}`);
+app.get("/tasks", (req, res) => {
+  db.all("SELECT * FROM tasks", (err, rows) => {
+    serverError(err, res);
+
+    return res.status(200).json(rows);
+  });
+
+  // return res.status(200).json(tasks);
+});
+
+app.post("/tasks", (req, res) => {
+  const newTask = req.body;
+
+  // tasks.push(newTask);
+
+  db.run("INSERT INTO tasks (text) VALUES (?)", [newTask.text], (err) => {
+    serverError(err, res);
+
+    return res.status(201).json({ id: this.lastID });
+  });
+});
+
+app.put("/tasks/:id", (req, res) => {
+  const { text } = req.body;
+  const taskId = parseInt(req.params.id);
+
+  db.run("UPDATE tasks SET text = ? WHERE id = ?", [text, taskId], (err) => {
+    serverError(err, res);
+    return res.status(200).json({ id: taskId, text });
+  });
+
+  // const foundTask = tasks.find((task) => task.id === taskId);
+
+  // if (!foundTask) {
+  //   return res.status(404).json({ message: "Did not find searched task" });
+  // }
+
+  // foundTask.text = updatedTask.text;
+});
+
+app.get("/tasks/:id", (req, res) => {
+  const taskId = parseInt(req.params.id);
+
+  db.get("SELECT * FROM tasks SET WHERE id = ?", taskId, (err, row) => {
+    serverError(err, res);
+    checkExist(row, res);
+
+    return res.status(200).json(row);
+  });
+
+  // const foundTask = tasks.find((task) => task.id === taskId);
+
+  // if (!foundTask) {
+  //   return res.status(404).json({ message: "Did not find searched task" });
+  // }
+
+  // foundTask.text = updatedTask.text;
+});
+
+app.delete("/tasks/:id", (req, res) => {
+  // const deleteTaskId = parseInt(req.params.id);
+  // tasks = tasks.filter((task) => task.id !== deleteTaskId);
+  // return res.status(204).json(tasks);
+
+  const taskId = parseInt(req.params.id);
+
+  db.run("DELETE from tasks WHERE id = ?", taskId, (err) => {
+    serverError(err, res);
+
+    return res.status(204).send();
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server's running on http://localhost:${port}`);
 });
